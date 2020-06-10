7a5fd7f4429d2c0ba0d287fab721947b1c3f81d7
YouHaveTrouble <garrenpolska@gmail.com>
2020-06-10 23:19:50 +0200

    added error message in console if the plugin is misconfigured

--- src/main/java/eu/endermite/effectsonhit/listeners/EntityDamageByEntityEvent.java
+++ src/main/java/eu/endermite/effectsonhit/listeners/EntityDamageByEntityEvent.java
@@ -13,43 +13,42 @@ import org.bukkit.potion.PotionEffectType;
 
 public class EntityDamageByEntityEvent implements Listener {
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onEntityDamageByEntity(org.bukkit.event.entity.EntityDamageByEntityEvent event) {
-        if (!event.isCancelled()) {
-            if (event.getDamager() instanceof Player && event.getEntity() instanceof LivingEntity) {
-                Player attacker = (Player) event.getDamager();
-                LivingEntity victim = (LivingEntity) event.getEntity();
-                if (Bukkit.getVersion().contains("1.8")) {
-                    if (attacker.getInventory().getItemInHand() != null) {
-                        if (attacker.getInventory().getItemInHand().hasItemMeta()) {
-                            for (String l : attacker.getInventory().getItemInHand().getItemMeta().getLore()) {
-                                if (EffectsOnHit.getPlugin().getCache().hasEnchant(l)) {
-                                    String effect = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchEffect();
-                                    int duration = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchDuration();
-                                    int amplify = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchamplify()-1;
-                                    new PotionEffect(PotionEffectType.getByName(effect.toUpperCase()), duration, amplify).apply(victim);
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    if (attacker.getInventory().getItemInMainHand() != null) {
-                        if (attacker.getInventory().getItemInMainHand().hasItemMeta()) {
-                            for (String l : attacker.getInventory().getItemInMainHand().getItemMeta().getLore()) {
-                                if (EffectsOnHit.getPlugin().getCache().hasEnchant(l)) {
-                                    String effect = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchEffect();
-                                    int duration = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchDuration();
-                                    int amplify = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchamplify()-1;
-                                    new PotionEffect(PotionEffectType.getByName(effect.toUpperCase()), duration, amplify).apply(victim);
-                                }
-                            }
-
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onEntityDamageByEntity(org.bukkit.event.entity.EntityDamageByEntityEvent event) {
+		if (!event.isCancelled()) {
+			if (event.getDamager() instanceof Player && event.getEntity() instanceof LivingEntity) {
+				Player attacker = (Player) event.getDamager();
+				LivingEntity victim = (LivingEntity) event.getEntity();
+				try {
+					if (Bukkit.getVersion().contains("1.8")) {
+						attacker.getInventory().getItemInHand();
+						if (attacker.getInventory().getItemInHand().hasItemMeta()) {
+							for (String l : attacker.getInventory().getItemInHand().getItemMeta().getLore()) {
+								if (EffectsOnHit.getPlugin().getCache().hasEnchant(l)) {
+									String effect = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchEffect();
+									int duration = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchDuration();
+									int amplify = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchamplify() - 1;
+									new PotionEffect(PotionEffectType.getByName(effect.toUpperCase()), duration, amplify).apply(victim);
+								}
+							}
+						}
+					} else {
+						attacker.getInventory().getItemInMainHand();
+						if (attacker.getInventory().getItemInMainHand().hasItemMeta()) {
+							for (String l : attacker.getInventory().getItemInMainHand().getItemMeta().getLore()) {
+								if (EffectsOnHit.getPlugin().getCache().hasEnchant(l)) {
+									String effect = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchEffect();
+									int duration = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchDuration();
+									int amplify = EffectsOnHit.getPlugin().getCache().getEnchant(l).getEnchamplify() - 1;
+									new PotionEffect(PotionEffectType.getByName(effect.toUpperCase()), duration, amplify).apply(victim);
+								}
+							}
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("Could not find specified effect. This is most likely caused by misconfiguring a plugin.");
+				}
+			}
+		}
+	}
 }
